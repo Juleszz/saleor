6c5eb8038d4645d5295cff9b3cf46670a9c5594b
JulianAtTheFrontend <92525148+JulianAtTheFrontend@users.noreply.github.com>
2021-10-28 11:23:15 +0200

    Update middleware.py

--- saleor/core/middleware.py
+++ saleor/core/middleware.py
@@ -113,6 +113,7 @@ def jwt_refresh_token_middleware(get_response):
                 expires=expires,
                 httponly=True,  # protects token from leaking
                 secure=secure,
+                domain="chalk.de"
                 samesite="None" if secure else "Lax",
             )
         return response
