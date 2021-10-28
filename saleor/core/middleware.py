8604e82306a9b89c12fdf1707bf0bfaeb2097718
JulianAtTheFrontend <92525148+JulianAtTheFrontend@users.noreply.github.com>
2021-10-28 11:13:39 +0200

    test2

--- saleor/core/middleware.py
+++ saleor/core/middleware.py
@@ -113,7 +113,6 @@ def jwt_refresh_token_middleware(get_response):
                 expires=expires,
                 httponly=True,  # protects token from leaking
                 secure=secure,
-                domain="https://rewhite-86006--beta-37alvlew.web.app",
                 samesite="None" if secure else "Lax",
             )
         return response
